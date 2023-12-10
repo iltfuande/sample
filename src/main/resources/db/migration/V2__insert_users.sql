db1b5474bffcddfcdad963e8fde59b5a40bf149a
iltfuande <iltfuande@gmail.com>
2023-12-11 00:57:59 +0200

    Update insert users sql file

--- src/main/resources/db/migration/V2__insert_users.sql
+++ src/main/resources/db/migration/V2__insert_users.sql
@@ -1,11 +1,11 @@
-INSERT INTO "user" (username, email) VALUES
-                                         ('john_doe', 'john.doe@example.com'),
-                                         ('alice_smith', 'alice.smith@example.com'),
-                                         ('bob_jenkins', 'bob.jenkins@example.com'),
-                                         ('emma_watson', 'emma.watson@example.com'),
-                                         ('michael_jones', 'michael.jones@example.com'),
-                                         ('olivia_perez', 'olivia.perez@example.com'),
-                                         ('david_miller', 'david.miller@example.com'),
-                                         ('sophia_garcia', 'sophia.garcia@example.com'),
-                                         ('william_taylor', 'william.taylor@example.com'),
-                                         ('amelia_anderson', 'amelia.anderson@example.com');
+INSERT INTO "user" (username, email)
+VALUES ('john_doe', 'john.doe@example.com'),
+       ('alice_smith', 'alice.smith@example.com'),
+       ('bob_jenkins', 'bob.jenkins@example.com'),
+       ('emma_watson', 'emma.watson@example.com'),
+       ('michael_jones', 'michael.jones@example.com'),
+       ('olivia_perez', 'olivia.perez@example.com'),
+       ('david_miller', 'david.miller@example.com'),
+       ('sophia_garcia', 'sophia.garcia@example.com'),
+       ('william_taylor', 'william.taylor@example.com'),
+       ('amelia_anderson', 'amelia.anderson@example.com');
